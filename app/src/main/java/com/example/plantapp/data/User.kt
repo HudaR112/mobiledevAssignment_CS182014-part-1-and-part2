9c8648dec65771a5e58b1726d36be41b890b6581
Huda Rauf <hudarauf112.hr@gmail.com>
2023-01-26 23:04:45 +0500

    first commit

---
+++ app/src/main/java/com/example/plantapp/data/User.kt
@@ -0,0 +1,16 @@
+package com.example.plantapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+
+    val id: Int,
+    val PlantName: String,
+    val PlantDescription: String,
+
+)
+
+
